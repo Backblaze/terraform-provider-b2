0976325d1cbf589cb0999e78d17e06a4e9f87e37
Maciej Szumocki <maciej.szumocki@reef.pl>
2021-05-28 17:32:32 +0200

    Python wrapper cleanup

--- python-bindings/b2_terraform/provider_tool.py
+++ python-bindings/b2_terraform/provider_tool.py
@@ -16,11 +16,12 @@ import traceback
 
 from class_registry import ClassRegistry
 from humps import camelize, decamelize
-from b2sdk.v1 import EncryptionAlgorithm, EncryptionMode, EncryptionSetting, BucketRetentionSetting
+from b2sdk.v1 import BucketRetentionSetting, EncryptionAlgorithm, EncryptionMode, EncryptionSetting
 
 from b2_terraform.api_wrapper import B2ApiWrapper
 from b2_terraform.arg_parser import ArgumentParser
 from b2_terraform.json_encoder import B2ProviderJsonEncoder
+from b2_terraform.terraform_structures import BUCKET_KEYS, FILE_KEYS, FILE_VERSION_KEYS, FILES_KEYS
 
 
 def change_keys(obj, converter):
@@ -384,76 +385,6 @@ class BucketFileVersion(Command):
 
     def _postprocess(self, **kwargs):
         return convert_json_to_go(kwargs, FILE_VERSION_KEYS)
-
-
-SERVER_SIDE_ENCRYPTION = {
-    "mode": True,
-    "algorithm": True,
-}
-
-FILE_VERSION_KEYS = {
-    "action": True,
-    "content_md5": True,
-    "content_sha1": True,
-    "content_type": True,
-    "bucket_id": True,
-    "file_id": True,
-    "file_info": True,
-    "file_name": True,
-    "size": True,
-    "source": True,
-    "server_side_encryption": SERVER_SIDE_ENCRYPTION,
-    "upload_timestamp": True,
-}
-
-FILE_KEYS = {
-    "bucket_id": True,
-    "file_name": True,
-    "show_versions": True,
-    "file_versions": FILE_VERSION_KEYS,
-}
-
-FILES_KEYS = {
-    "bucket_id": True,
-    "folder_name": True,
-    "show_versions": True,
-    "recursive": True,
-    "file_versions": FILE_VERSION_KEYS,
-}
-
-BUCKET_KEYS = {
-    "bucket_id": True,
-    "bucket_name": True,
-    "bucket_type": True,
-    "bucket_info": True,
-    "cors_rules": {
-        "cors_rule_name": True,
-        "allowed_origins": True,
-        "allowed_operations": True,
-        "max_age_seconds": True,
-        "allowed_headers": True,
-        "expose_headers": True,
-    },
-    "file_lock_configuration": {
-        "is_file_lock_enabled": True,
-        "default_retention": {
-            "mode": True,
-            "period": {
-                "duration": True,
-                "unit": True,
-            },
-        },
-    },
-    "default_server_side_encryption": SERVER_SIDE_ENCRYPTION,
-    "lifecycle_rules": {
-        "file_name_prefix": True,
-        "days_from_hiding_to_deleting": True,
-        "days_from_uploading_to_hiding": True,
-    },
-    "account_id": True,
-    "options": True,
-    "revision": True,
-}
 
 
 @B2Provider.register_subcommand
